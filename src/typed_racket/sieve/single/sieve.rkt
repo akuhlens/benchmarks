945845f08f2b3eecbe756a378b2bd1edd35aa9ce
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-11-15 18:44:52 -0500

    progress so far

--- src/typed_racket/sieve/single/sieve.rkt
+++ src/typed_racket/sieve/single/sieve.rkt
@@ -3,7 +3,33 @@
 ;; Use the partner file "streams.rkt" to implement the Sieve of Eratosthenes.
 ;; Then compute and print the 10,000th prime number.
 
-(require "streams.rkt")
+;; ;; A stream is a cons of a value and a thunk that computes the next value when applied
+(define-type stream (Rec s (Pair Natural (-> s))))
+
+(: stream-first : stream -> Natural)
+(define stream-first car)
+
+(: stream-rest : stream -> stream)
+(define (stream-rest st) ((cdr st)))
+
+;;--------------------------------------------------------------------------------------------------
+
+(: make-stream (-> Natural (-> stream) stream))
+(define (make-stream hd thunk)
+  (cons hd thunk))
+
+;; Destruct a stream into its first value and the new stream produced
+;; by de-thunking the tail
+(: stream-unfold (-> stream (Pair Natural stream)))
+(define (stream-unfold st)
+  (cons (stream-first st) (stream-rest st)))
+
+;; [stream-get st i] Get the [i]-th element from the stream [st]
+(: stream-get (-> stream Natural Natural))
+(define (stream-get st i)
+  (cond
+    [(= i 0) (stream-first st)]
+    [else (stream-get (stream-rest st) (sub1 i))]))
 
 ;;--------------------------------------------------------------------------------------------------
 
