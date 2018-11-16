52a901dc9d9958f3848765ff5f4df3fd0c38cb5b
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-11-16 15:48:27 -0500

    n_body

--- src/racket/n_body/modules/system.rkt
+++ src/racket/n_body/modules/system.rkt
@@ -1,26 +1,27 @@
-#lang typed/racket/base
+#lang racket/base
 
 (require racket/flonum)
 
-(require/typed/check "body.rkt"
-  [make-body (Flonum Flonum Flonum Flonum Flonum Flonum Flonum -> Body)])
+(require "body.rkt")
 
 (provide +solar-mass+
-        +dt+
-        *system*
-        *system-size* )
+         +dt+
+         *system*
+         *system-size* )
 
-(define +pi+ : Flonum 3.141592653589793) ;; define locally to enable inlining
-(define +days-per-year+ : Flonum 365.24)
+;; define planetary masses, initial positions & velocity
 
-(define +solar-mass+ : Flonum (fl* 4.0 (fl* +pi+ +pi+)))
+(define +pi+ 3.141592653589793) ;; define locally to enable inlining
+(define +days-per-year+ 365.24)
 
-(define +dt+ : Flonum 0.01)
+(define +solar-mass+ (fl* 4.0 (fl* +pi+ +pi+)))
 
-(define *sun* : Body
+(define +dt+ 0.01)
+
+(define *sun*
   (make-body 0.0 0.0 0.0 0.0 0.0 0.0 +solar-mass+))
 
-(define *jupiter* : Body
+(define *jupiter*
   (make-body 4.84143144246472090
              -1.16032004402742839
              -1.03622044471123109e-1
@@ -29,7 +30,7 @@
              (fl* -6.90460016972063023e-5 +days-per-year+)
              (fl* 9.54791938424326609e-4 +solar-mass+)))
 
-(define *saturn* : Body
+(define *saturn*
   (make-body 8.34336671824457987
              4.12479856412430479
              -4.03523417114321381e-1
@@ -38,7 +39,7 @@
              (fl* 2.30417297573763929e-5 +days-per-year+)
              (fl* 2.85885980666130812e-4 +solar-mass+)))
 
-(define *uranus* : Body
+(define *uranus*
   (make-body 1.28943695621391310e1
              -1.51111514016986312e1
              -2.23307578892655734e-1
@@ -47,7 +48,7 @@
              (fl* -2.96589568540237556e-05 +days-per-year+)
              (fl*  4.36624404335156298e-05 +solar-mass+)))
 
-(define *neptune* : Body
+(define *neptune*
   (make-body 1.53796971148509165e+01
              -2.59193146099879641e+01
              1.79258772950371181e-01
@@ -56,6 +57,6 @@
              (fl* -9.51592254519715870e-05 +days-per-year+)
              (fl* 5.15138902046611451e-05 +solar-mass+)))
 
-(define-type System (Vector Body Body Body Body Body))
-(define *system* : System (vector *sun* *jupiter* *saturn* *uranus* *neptune*))
-(define *system-size* : Integer 5)
+(define *system* (vector *sun* *jupiter* *saturn* *uranus* *neptune*))
+(define *system-size* 5)
+
--- src/typed_racket/n_body/modules/energy.rkt
+++ src/typed_racket/n_body/modules/energy.rkt
@@ -2,6 +2,8 @@
 
 (require racket/flonum)
 
+(define-type Body (Vector Flonum Flonum Flonum Flonum Flonum Flonum Flonum))
+
 (require/typed/check "system.rkt"
   [+solar-mass+ Flonum]
   [+dt+ Flonum]
@@ -10,24 +12,25 @@
 
 (provide energy)
 
+(: energy : -> Flonum)
 (define (energy)
   (let loop-o : Flonum ([o : Integer 0] [e : Flonum 0.0]) 
        (if (= o *system-size*)
            e
            (let* ([o1 (vector-ref *system* o)]
-                  [e (fl+ e (fl* (fl* 0.5 (body-mass o1))
-                                 (fl+ (fl+ (fl* (body-vx o1) (body-vx o1))
-                                           (fl* (body-vy o1) (body-vy o1)))
-                                      (fl* (body-vz o1) (body-vz o1)))))])
+                  [e (fl+ e (fl* (fl* 0.5 (vector-ref o1 6))
+                                 (fl+ (fl+ (fl* (vector-ref o1 3) (vector-ref o1 3))
+                                           (fl* (vector-ref o1 4) (vector-ref o1 4)))
+                                      (fl* (vector-ref o1 5) (vector-ref o1 5)))))])
              (let loop-i : Flonum ([i : Integer (+ o 1)] [e : Flonum e])
                   (if (= i *system-size*)
                       (loop-o (+ o 1) e)
                       (let* ([i1   (vector-ref *system* i)]
-                             [dx   (fl- (body-x o1) (body-x i1))]
-                             [dy   (fl- (body-y o1) (body-y i1))]
-                             [dz   (fl- (body-z o1) (body-z i1))]
+                             [dx   (fl- (vector-ref o1 0) (vector-ref i1 0))]
+                             [dy   (fl- (vector-ref o1 1) (vector-ref i1 1))]
+                             [dz   (fl- (vector-ref o1 2) (vector-ref i1 2))]
                              [dist (flsqrt (fl+ (fl+ (fl* dx dx) (fl* dy dy)) (fl* dz dz)))]
-                             [e    (fl- e (fl/ (fl* (body-mass o1) (body-mass i1)) dist))])
+                             [e    (fl- e (fl/ (fl* (vector-ref o1 6) (vector-ref i1 6)) dist))])
                         (loop-i (+ i 1) e))))))))
 
 
