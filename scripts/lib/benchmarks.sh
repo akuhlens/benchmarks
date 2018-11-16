a5109d51f2f68e87e100a58fb9a88ae271375846
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-11-16 18:20:59 -0500

    reordering

--- scripts/lib/benchmarks.sh
+++ scripts/lib/benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-declare -a BENCHMARKS=(sieve fft blackscholes n_body quicksort  matmult tak ray array)
+declare -a BENCHMARKS=(sieve fft blackscholes n_body ray quicksort matmult tak array)
 
-declare -a BENCHMARKS_ARGS_LATTICE=("fast.txt" "medium1.txt" "in_4K.txt" "slow.txt" "in_descend1000.txt" "400.txt" "slow.txt" "empty.txt" "slow.txt")
+declare -a BENCHMARKS_ARGS_LATTICE=("fast.txt" "medium1.txt" "in_4K.txt" "slow.txt" "empty.txt" "in_descend1000.txt" "400.txt" "slow.txt" "slow.txt")
 
-declare -a BENCHMARKS_ARGS_EXTREMES=("slow.txt" "slow.txt" "in_16K.txt" "slow.txt" "in_descend10000.txt" "400.txt" "slow.txt" "empty.txt" "slow.txt")
+declare -a BENCHMARKS_ARGS_EXTREMES=("slow.txt" "slow.txt" "in_16K.txt" "slow.txt" "empty.txt" "in_descend10000.txt" "400.txt" "slow.txt" "slow.txt")
